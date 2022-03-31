f2e60f4782204a4e671e1f870595704f5d68ffd8
Denllay <denllaay@gmail.com>
2022-03-31 19:42:31 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import styles from './App.module.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+    const [address, setAddress] = useState(null);
 
-export default App;
+    const onMetamaskLogin = async () => {
+        if (window.ethereum) {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+            setAddress(window.web3.currentProvider.selectedAddress);
+        }
+    };
+
+    useEffect(() => {
+        window.web3 = new Web3(window.ethereum);
+
+        if (window.ethereum) {
+            window.web3.eth.getAccounts(function (err, accounts) {
+                if (accounts.length !== 0) {
+                    setAddress(accounts[0]);
+                }
+            });
+        }
+    }, []);
+
+    return (
+        <div className={styles.app}>
+            <div className={styles.container}>
+                {address ? (
+                    <span className={styles.address}>
+                        Your address: <b>{address}</b>
+                    </span>
+                ) : (
+                    <>
+                        <h1 className={styles.title}>MetaMask is locked - please login</h1>
+
+                        <button className={styles.button} onClick={onMetamaskLogin}>
+                            Get public key
+                        </button>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+};
